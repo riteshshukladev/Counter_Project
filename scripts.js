a8e55b177c6304be429f34c2f5e491b2037a284d
riteshshukladev <108568521+riteshshukladev@users.noreply.github.com>
2023-03-13 13:44:45 +0530

    Done with the code

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let count=0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".buttn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+       const styles = e.currentTarget.classList;
+       console.log(styles); 
+    //    increase decrease reset
+       if(styles.contains("decrease")){
+           count--;
+           value.innerHTML = count;
+       }
+         else if(styles.contains("increase")){
+            count++;
+            value.innerHTML = count;
+         }
+         else{
+                count=0;
+                value.innerHTML = count;
+         }
+        //  color change
+         if(count>0){
+            value.style.color="green";
+         }
+         else if(count<0){
+            value.style.color="red";
+         }
+            else{
+                value.style.color="black";
+            }
+    })
+})
